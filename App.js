199842ba0dcc57747f3b7da8cc292619a6c2da4e
Long Phuc Loc <longphucloc123@gmail.com>
2023-11-13 22:01:19 +0700

    Set up project Movie Ticket Booking

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AntDesign from 'react-native-vector-icons/AntDesign'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
