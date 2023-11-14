0d062ae35f2ee90a331f605f7ecb595a8abe9b37
Long Phuc Loc <longphucloc123@gmail.com>
2023-11-15 00:47:46 +0700

    Create TabNavigator

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { searchMovies } from './src/api/apicalls'
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TabNavigator from './src/navigators/TabNavigator';
+import MovieDetailsScreen from './src/screens/MovieDetailsScreen';
+import SeatBookingScreen from './src/screens/SeatBookingScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  console.log(searchMovies('Avengers'))
   return (
-    <View>
-      <Text>App</Text>
-
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Tab' component={TabNavigator} options={{ animation: 'default' }} />
+        <Stack.Screen name='MovieDetails' component={MovieDetailsScreen} options={{ animation: 'slide_from_right' }} />
+        <Stack.Screen name='SeatBooking' component={SeatBookingScreen} options={{ animation: 'slide_from_bottom' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
-export default App
-
-const styles = StyleSheet.create({})
+export default App;
