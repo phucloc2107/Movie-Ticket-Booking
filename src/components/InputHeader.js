4d06b84c6c28da544905cea651aa70f5216067d2
Long Phuc Loc <longphucloc123@gmail.com>
2023-11-18 17:06:00 +0700

    Create HomeScreen

---
+++ src/components/InputHeader.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+const InputHeader = (props) => {
+
+    const [searchText, setSearchText] = useState('');
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                style={styles.textInput}
+                onChangeText={textInput => setSearchText(textInput)}
+                value={searchText}
+                placeholder='Search your Movies...'
+                placeholderTextColor={COLORS.WhiteRGBA32}
+            />
+
+            <TouchableOpacity style={styles.searchIcon} onPress={() => props.searchFunction(searchText)}>
+                <FontAwesome name='search' color={COLORS.Orange} size={FONTSIZE.size_20} />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default InputHeader
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        paddingVertical: SPACING.space_8,
+        paddingHorizontal: SPACING.space_24,
+        borderWidth: 2,
+        borderColor: COLORS.WhiteRGBA15,
+        borderRadius: BORDERRADIUS.radius_25,
+        flexDirection: 'row'
+    },
+    textInput: {
+        width: '90%',
+        fontFamily: FONTFAMILY.poppins_regular,
+        fontSize: FONTSIZE.size_14,
+        color: COLORS.White
+    },
+    searchIcon: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: SPACING.space_10
+    }
+});
---
+++ src/components/SubMovieCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
+
+const SubMovieCard = (props) => {
+    return (
+        <TouchableOpacity onPress={() => props.cardFunction()}>
+            <View style={[
+                styles.container,
+                props.shoudlMarginatedAtEnd
+                    ? props.isFirst ? { marginLeft: SPACING.space_36 }
+                        : props.isLast ?
+                            { marginRight: SPACING.space_36 }
+                            : {}
+                    : {},
+                props.shouldMarginateAround ? { margin: SPACING.space_12 } : {}, { maxWidth: props.cardWidth }
+            ]}>
+                <Image
+                    source={{ uri: props.imagePath }}
+                    style={[styles.cardImage, { width: props.cardWidth }]}
+                />
+                <Text numberOfLines={1} style={styles.textTitle}>{props.title}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flex: 1,
+        backgroundColor: COLORS.Black
+    },
+    cardImage: {
+        aspectRatio: 2 / 3,
+        borderRadius: BORDERRADIUS.radius_20,
+    },
+    textTitle: {
+        fontFamily: FONTFAMILY.poppins_regular,
+        fontSize: FONTSIZE.size_14,
+        color: COLORS.White,
+        textAlign: 'center',
+        paddingVertical: SPACING.space_10
+    }
+})
+
+export default SubMovieCard
